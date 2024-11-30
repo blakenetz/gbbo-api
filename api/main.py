9f27ec0f72599b52d5d8a49442dcbf9ae6c07466
Blake Netzeband <blake.netzeband@gmail.com>
2024-11-30 10:31:28 -0700

    update return shape. merge tables programatically

--- api/main.py
+++ api/main.py
@@ -1,11 +1,17 @@
-from typing import Annotated, TypedDict, Union
+from typing import Annotated, Union
 from fastapi import FastAPI, HTTPException, Query, Depends
 import logging 
-from sqlmodel import Field, Session, SQLModel, create_engine, select, UniqueConstraint
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select, UniqueConstraint
+
+class RecipeDiet(SQLModel, table=True, extend_existing=True):
+  __tablename__ = 'recipe_diets'
+  __table_args__ = (UniqueConstraint('recipe_id', 'diet_id'),)
+  id: Union[int, None] = Field(default=None, primary_key=True)
+  recipe_id: int = Field(foreign_key="recipes.id", nullable=False)
+  diet_id: int = Field(foreign_key="diets.id", nullable=False)
 
 class Recipe(SQLModel, table=True, extend_existing=True):
   __tablename__ = 'recipes'
-
   id: Union[int, None] = Field(default=None, primary_key=True)
   title: str = Field(nullable=False)
   link: str = Field(nullable=False, unique=True)
@@ -14,28 +20,22 @@ class Recipe(SQLModel, table=True, extend_existing=True):
   is_technical: bool = Field(nullable=False)
   time: Union[int, None]
   baker_id: Union[int, None] = Field(default=None, foreign_key="bakers.id")
+  baker: Union['Baker', None] = Relationship(back_populates='recipes')
+  # diets: Union[list['Diet'], None] = Relationship(back_populates="recipes", link_model=RecipeDiet)
 
 class Diet(SQLModel, table=True, extend_existing=True):
   __tablename__ = 'diets'
-
   id: Union[int, None] = Field(default=None, primary_key=True)
   name: str = Field(nullable=False, unique=True)
+  # recipes: Union[list['Recipe'], None] = Relationship(back_populates="diets", link_model=RecipeDiet)
 
 class Baker(SQLModel, table=True, extend_existing=True):
   __tablename__ = 'bakers'
   __table_args__ = (UniqueConstraint('name', 'img'),)
-
   id: Union[int, None] = Field(default=None, primary_key=True)
   name: str = Field(nullable=False)
   img: str = Field(nullable=False)
-
-class RecipeDiet(SQLModel, table=True, extend_existing=True):
-  __tablename__ = 'recipe_diets'
-  __table_args__ = (UniqueConstraint('recipe_id', 'diet_id'),)
-  
-  id: Union[int, None] = Field(default=None, primary_key=True)
-  recipe_id: int = Field(foreign_key="recipes.id", nullable=False)
-  diet_id: int = Field(foreign_key="diets.id", nullable=False)
+  recipes: list['Recipe'] = Relationship(back_populates='baker')
 
 # Configure logging
 logging.basicConfig(
@@ -57,6 +57,11 @@ def get_session():
   with Session(engine) as session:
     yield session
 
+def parse_recipe(recipe: Recipe, baker: Baker):
+  data = recipe.model_dump(exclude={'baker_id'})
+  data['baker'] = baker.model_dump(exclude={"recipes"}) if baker else None
+  return data
+
 SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
@@ -67,39 +72,53 @@ def on_startup():
 
 @app.get("/")
 def read_root():
-  return {"data": [], 'message': 'Greetings from the unofficial GBBO API!'}
+  return 'Greetings from the unofficial GBBO API!'
 
 @app.get("/recipes")
 def get_recipes(
   session: SessionDep,
-  q: Annotated[Union[str, None], Query(description="Case insensitive search against recipe title")] = None, 
-  limit: Union[int, None] = 50, 
-  skip: Union[int, None] = 0, 
-  baker_ids: Annotated[list[int], Query(description="List of baker ids. Available at GET /bakers")] = None, 
+  limit: Union[int, None] = 50,
+  skip: Union[int, None] = 0,
+  q: Annotated[Union[str, None], Query(description="Case insensitive search against recipe title")] = None,
+  difficulty: Annotated[Union[int, None], Query(le=3, ge=1, description="Difficulty on a 1-3 scale")] = None,
+  is_technical: Annotated[Union[bool, None], Query(description="Filter by technical bakes only")] = None,
+  time: Annotated[Union[int, None], Query(description="Max time in minutes")] = None,
+  baker_ids: Annotated[list[int], Query(description="List of baker ids. Available at GET /bakers")] = None,
   diet_ids: Annotated[list[int], Query(description="List of diet ids. Available at GET /diets")] = None,
-  difficulty: Annotated[Union[int, None], Query(le=3, ge=1, description="Difficulty on a 1-3 scale")] = None, 
-  is_technical: Annotated[Union[bool, None], Query(description="Filter by technical bakes only")] = None, 
-  time: Annotated[Union[int, None], Query(description="Max time in minutes")] = None, 
 ): 
-  statement = select(Recipe).offset(skip).limit(limit)
+  statement = select(Recipe, Baker).join(Baker, isouter=True).offset(skip).limit(limit)
+  
   if (q):
     statement = statement.where(Recipe.title.contains(q))
-  if (baker_ids):
-    statement = statement.where(Recipe.baker_id.in_(baker_ids))
   if (difficulty):
     statement = statement.where(Recipe.difficulty == difficulty)
   if (is_technical):
     statement = statement.where(Recipe.is_technical == is_technical)
   if (time):
     statement = statement.where(Recipe.time <= time)
+  if (baker_ids):
+    statement = statement.where(Recipe.baker_id.in_(baker_ids))
 
   results = session.exec(statement).all()
-  return {"data": list(results)}
+  data = []
+  for recipe, baker in results:
+    recipe_data = parse_recipe(recipe, baker)
+    data.append(recipe_data)
+
+  return data
 
 @app.get("/recipe/{recipe_id}")
 def get_recipe(recipe_id: int, session: SessionDep):
-  result = session.get(Recipe, recipe_id)
-  return {"data": result}
+  statement = select(Recipe, Baker).where(Recipe.id == recipe_id).join(Baker, isouter=True)
+  result = session.exec(statement).first()
+  
+  if not result:
+    raise HTTPException(status_code=404, detail="Recipe not found")
+  
+  recipe, baker = result
+  data = parse_recipe(recipe, baker)
+
+  return data
 
 @app.get("/bakers")
 def get_bakers(
@@ -111,12 +130,12 @@ def get_bakers(
     statement = statement.where(Baker.name.contains(q))
   
   results = session.exec(statement).all()
-  return {"data": list(results)}
+  return list(results)
 
 @app.get("/baker/{baker_id}")
 def get_baker(baker_id: int, session: SessionDep):
   result = session.get(Baker, baker_id)
-  return {"data": result}
+  return result
 
 @app.get("/diets")
 def get_diets(
@@ -128,11 +147,11 @@ def get_diets(
     statement = statement.where(Diet.name.contains(q))
   
   results = session.exec(statement).all()
-  return {"data": list(results)}
+  return list(results)
 
 @app.get("/diet/{diet_id}")
 def get_diet(diet_id: int, session: SessionDep):
   result = session.get(Diet, diet_id)
-  return {'data': result}
+  return result
 
 
