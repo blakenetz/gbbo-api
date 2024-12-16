9cb9f13b1cec353832aa9fc3a5eda872bd4c055c
Blake Netzeband <blake.netzeband@gmail.com>
2024-12-16 14:16:30 -0700

    implement caching

--- packages/frontend/util/index.ts
+++ packages/frontend/util/index.ts
@@ -2,3 +2,10 @@ export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export const paginationAmount = 25;
+
+export const cacheConfig: RequestInit = {
+  cache: "force-cache",
+  next: {
+    revalidate: 60 * 60, // 1 hour
+  },
+};
